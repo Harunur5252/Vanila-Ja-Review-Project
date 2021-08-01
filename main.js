39e541bc5d4564e202d33bc62fb744e1bb9ca2a9
Harunur5252 <hroshid695@gmil.com>
2021-08-01 15:55:00 +0600

    some changed index.html file and create server folder

--- main.js
+++ main.js
@@ -1,65 +1,85 @@
+class ReviewData{
+  currentItem = 0
+  reviews=[]
+
+  async getData(){
+    const res = await fetch('http://localhost:3000/reviews')
+    return await res.json()
+  }
+  apiData(){
+    this.getData().then(elm=>{
+      this.reviews.push(...elm)
+    }).catch(err=>{
+       console.log(new Error(err))
+    })
+  }
+  getRightIconData(){
+    this.currentItem++
+    if(this.currentItem>this.reviews.length-1){
+      this.currentItem = 0
+    }
+    ui.showPerson(this.currentItem)
+  }
+  getLeftIconData(){
+    this.currentItem--
+    if (this.currentItem < 0) {
+      this.currentItem = this.reviews.length - 1;
+    }
+    ui.showPerson(this.currentItem)
+  }
+  getRandomValue(){
+    const randomValue = Math.floor(Math.random() * this.reviews.length)
+    ui.showPerson(randomValue)
+  }
+  domLoaded(){
+    ui.showPerson(this.currentItem)
+  }
+}
+const data = new ReviewData()
+data.apiData()
 
 
-const setItemElm  = document.querySelector('.setItem')
-const cirImgElm  = document.querySelector('.cirImg')
-const nameElm  = document.querySelector('.name')
-const titleElm  = document.querySelector('.title')
-const textElm  = document.querySelector('.text')
-const leftIconElm  = document.querySelector('.leftIcon')
-const rightIconElm  = document.querySelector('.rightIcon')
-const sBtnElm  = document.querySelector('.sBtn')
-
-let currentItem = 0
-let reviews = []
-async function getData(){
-  const res = await fetch('http://localhost:3000/reviews')
-  return await res.json()
+class UI{
+  selectors(){
+    const setItemElm  = document.querySelector('.setItem')
+    const cirImgElm  = document.querySelector('.cirImg')
+    const nameElm  = document.querySelector('.name')
+    const titleElm  = document.querySelector('.title')
+    const textElm  = document.querySelector('.text')
+    const leftIconElm  = document.querySelector('.leftIcon')
+    const rightIconElm  = document.querySelector('.rightIcon')
+    const sBtnElm  = document.querySelector('.sBtn')
+    return {
+      setItemElm,cirImgElm,nameElm,titleElm,textElm,leftIconElm,rightIconElm,sBtnElm
+    }
+  }
+  showPerson(item){
+      const {cirImgElm,nameElm,titleElm,textElm} = this.selectors()
+      const elm = data.reviews[item]
+      
+      cirImgElm.src=elm.image   
+      nameElm.textContent=elm.name
+      titleElm.textContent=elm.profession
+      textElm.textContent=elm.description
+  }
+  loadAllEventListener(){
+      const {rightIconElm,leftIconElm,sBtnElm} = this.selectors()
+      window.addEventListener('DOMContentLoaded',()=>data.domLoaded())
+      rightIconElm.addEventListener('click',()=>data.getRightIconData())
+      leftIconElm.addEventListener('click',()=>data.getLeftIconData())
+      sBtnElm.addEventListener('click',()=>data.getRandomValue())
+  }
 }
 
+const ui = new UI()
+ui.loadAllEventListener()
 
-getData().then(elm=>{
-  elm.forEach(item => {
-    reviews.push(item)
-  });
-}).catch(err=>{
-   console.log(new Error(err))
-})
 
-function showPerson(item){
-  const elm = reviews[item]
-  cirImgElm.src=elm.image   
-  nameElm.textContent=elm.name
-  titleElm.textContent=elm.profession
-  textElm.textContent=elm.description
-}
 
-window.addEventListener('DOMContentLoaded',()=>{
-  showPerson(currentItem)
-})
 
-rightIconElm.addEventListener('click',(e)=>{
-  
-  currentItem++
-  if(currentItem>reviews.length-1){
-    currentItem = 0
-  }
-  
-    showPerson(currentItem)
-})
 
-leftIconElm.addEventListener('click',(e)=>{
-  currentItem--
-  if (currentItem < 0) {
-    currentItem = reviews.length - 1;
-  }
-  
-    showPerson(currentItem)
-})
 
-sBtnElm.addEventListener('click',()=>{
-  currentItem = Math.floor(Math.random() * reviews.length)
-  showPerson(currentItem)
-})
+
 
 
 
